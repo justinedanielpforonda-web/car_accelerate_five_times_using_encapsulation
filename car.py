b9c552bdeed879210feece136e9b24c99c5d4242
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-06-05 07:42:04 +0800

    polished and added some error handling

--- car.py
+++ car.py
@@ -1,6 +1,13 @@
 class Car:
 
     def __init__(self, year_model, make):
+
+        if year_model <= 0:
+            raise ValueError("Invalid year model.")
+
+        if make == "":
+            raise ValueError("Make cannot be empty.")
+
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
@@ -9,23 +16,27 @@ class Car:
         self.__speed += 5
 
     def brake(self):
-        if self.__speed >= 5:
-            self.__speed -= 5
+
+        if self.__speed < 5:
+            raise ValueError("Cannot brake below zero.")
+
+        self.__speed -= 5
 
     def get_speed(self):
         return self.__speed
 
 
+# Create Car object
 car1 = Car(2025, "Toyota")
 
-print("Accelerating")
+print("=== Accelerating ===")
 
 for i in range(5):
     car1.accelerate()
-    print(car1.get_speed())
+    print(f"Speed after acceleration {i+1}: {car1.get_speed()}")
 
-print("Braking")
+print("\n=== Braking ===")
 
 for i in range(5):
     car1.brake()
-    print(car1.get_speed())
+    print(f"Speed after braking {i+1}: {car1.get_speed()}")
